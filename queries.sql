627a85634d54cba896d3b2f2674a380d9c090b6f
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-11 13:39:56 -0800

    Solve problems in queries.sql.

--- queries.sql
+++ queries.sql
@@ -2,8 +2,40 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+select p.productName as ProductName
+, c.categoryName as CategoryName
+from product as p
+join category as c
+on p.CategoryId = c.Id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+select o.id
+, s.companyName
+from [order] as o
+join shipper as s
+on o.ShipVia = s.id
+where o.OrderDate < '2012-08-08';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+select p.productName as name
+,o.quantity
+from orderDetail as o
+join product as p
+on o.productId = p.id
+where o.orderId = 10251
+order by p.productName
+limit 3;
+
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+select o.id as Order_Id
+, c.companyName as Customer_Company_Name
+,e.LastName as Employee_Last_Name
+from [order] as o 
+join customer as c 
+    on o.customerId = c.id 
+join employee as e 
+    on o.employeeId = e.id;
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,43 +1,51 @@
 const db = require('../data/dbConfig.js');
 module.exports = {
-find,
-findById,
-findSteps,
-add,
-update,
-remove,
+	find,
+	findById,
+	findSteps,
+	add,
+	update,
+	remove
 };
 
 function find() {
-return db('schemes');
+	return db('schemes');
 }
 
 function findById(id) {
-return db('schemes')
-.where({ id })
-.first();
+	return db('schemes')
+		.where({ id })
+		.first();
 }
 
 function findSteps(id) {
-    return db('schemes')
-
-}
-function add(post) {
-return db('schemes')
-.insert(post)
-.then(ids => {
-return findById(ids[0]);
-});
+	return db('steps')
+		.where({ id })
+		.first();
 }
 
-function update(id, changes) {
-return db('schemes')
-.where({ id })
-.update(changes);
+function add(scheme) {
+	return db('schemes')
+		.insert(scheme)
+		.then(ids => {
+			return findById(ids[0]);
+		});
+}
+
+function addStep(id) {
+	return db('schemes')
+		.findById(id)
+		.then(foundId => {});
+}
+
+function update(changes, id) {
+	return db('schemes')
+		.where({ id })
+		.update(changes);
 }
 
 function remove(id) {
-return db('schemes')
-.where('id', id)
-.del();
+	return db('schemes')
+		.where('id', id)
+		.del();
 }
